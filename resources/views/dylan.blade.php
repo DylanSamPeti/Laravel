ff8a3be2ace8749b717aec4e58baa88a104eb93c
Dylan Peti <dylanpetiinfo@gmail.com>
2015-06-17 05:46:40 +1200

    update profile

--- resources/views/dylan.blade.php
+++ resources/views/dylan.blade.php
@@ -1,45 +1,209 @@
 <html>
 	<head>
-		<title>Dylan Peti</title>
+		<title>Dylan</title>
 
 
 		<link href="{{ asset('/css/custom.css') }}" rel="stylesheet">
-		<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
-		<script src="{{ asset('/js/navigation.js') }}"></script>
-
 		
 		<link href='//fonts.googleapis.com/css?family=Lato:100' rel='stylesheet' type='text/css'>
 		<link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css" rel="stylesheet">
-		<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
 		<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script> 
 		<link  href="http://cdnjs.cloudflare.com/ajax/libs/fotorama/4.6.3/fotorama.css" rel="stylesheet"> <!-- 3 KB -->
         <script src="http://cdnjs.cloudflare.com/ajax/libs/fotorama/4.6.3/fotorama.js"></script> <!-- 16 KB -->
 
+		<style>
+			body {
+				margin: 0;
+				padding: 0;
+				width: 100%;
+				height: 100%;
+				color: #B0BEC5;
+				display: block;
+				font-weight: 100;
+				font-family: 'Lato';
+			}
+
+			.container {
+				text-align: center;
+				display: block;
+				margin: 0 auto;
+			}
+
+			.content {
+
+				position: relative;
+				top: 50%;
+				margin-top: 122.5px;
+				text-align: center;
+				display: inline-block;
+			}
+
+			.title {
+				font-size: 96px;
+				
+			}
+
+			.quote {
+				font-size: 24px;
+			}
+			#development{
+
+				
+
+			}
+		</style>
 	</head>
 	<body>
-
-		  <div class="container front-page">
+		<main id="main">
+		  <div class="container">
 			<div class="content">
-				<div class="title">D &nbsp Y &nbsp L &nbsp A &nbsp N</div>
-				<div class="quote">Design + Photography  + Art </div>
+				<div class="title">Dylan</div>
+				<div class="quote">design + Photography + Art</div>
 				<ul id="nav">
-				  <li><a href="about" data-scrollto="about">About</a></li>
-				  <li><a href="work" data-scrollto="work">Work</a></li>
-				  <li><a href="http://studio-dylan.tumblr.com" data-scrollto="no" target="_blank">Blog</a></li>
-<!-- 				  <li><a href="" data-scrollto="contact">Contact</a></li> -->
+				  <li data-goto><a href="">About</a></li>
+<!-- 				  <li><a href="">Work</a></li> -->
+				  <li><a href="http://studio-dylan.tumblr.com/">Blog</a></li>
+				  <li><a href="/contact">Contact</a></li>
 				</ul>
-           <br>
-           <br>
-				<!-- <button class="btn btn-primary"><a href="/survey">Take Survey</a></button> -->
             </div>
          </div>
+		</main>
 
-         <div class="container front-page about">
 
-          <div class=""></div>
-         	
+
+		<section id="secondary">
+			
+		  <div class="container">
+		  	<div class="content">
+				<h1>Work</h1>
+
+				<ul>
+
+					<li>
+
+				       <article>
+
+				       	<h3>The Blue</h3>
+    
+				       </article>
+
+				    </li>
+
+				    <li>
+
+				       <article>
+
+				       	<h3>The Red</h3>
+    
+				       </article>
+
+				    </li>
+
+				    <li>
+
+				       <article>
+                        <div id="overlay"></div>
+				       	<h3>Writings</h3>
+     
+				       </article>
+
+				    </li>
+				</ul>
+
+				
+            </div>
+		 <!--  	<div class="fotorama" data-nav="thumbs" data-allowfullscreen="true" data-width="1200"
+     data-height="400">
+                <img src="{{ asset('/images/img-one.jpg') }}">
+                <img src="{{ asset('/images/img-two.jpg') }}">
+                <img src="{{ asset('/images/img-three.jpg') }}">
+                <img src="{{ asset('/images/img-four.jpg') }}">
+                </div>
+ -->
+			
          </div>
+		</section>
 
+		<section id="tertiary" class="about">
+
+		  <div class="container">
+		  	<div class="content">
+
+             <h1>About Dylan</h1>
+             <br>
+             <br>
+             <h3>Dylan is a Creative Technologies undergraduate based at AUT University. He has gained experience <br>  in several programming languages and frameworks. With his focus on technical <br>marketing, Dylan is what some in the industry call a growth hacker.  
+             </h3>
+             <div id="mugshot">
+
+
+
+<!--              	<div id="overlay"> -->
+             		<div id="DP">
+             			
+             		<div id="profile-picture"></div>
+                 	
+                 	<div class="paragraph">
+             		<h3>Dylan Peti</h3>
+
+             		<p>
+           
+             			Phone: 02108276106
+             			Email: dylanpetiinfo@gmail.com
+             			</p>
+             		</div>
+
+
+
+             		
+             		</div>
+             		<div id="skills">
+             		<h1>Skills</h1>
+             		<ul>
+             		<li>Market Analysis</li>
+                    <li>Marketing Strategy</li>
+                    <li>Brand Development</li>
+                    <li>Branding Structure/Guidelines</li>
+                    <li>Brand Artwork Creation</li>
+                    <li>Website Design and Development</li>
+                    <li>Website Search Engine Optimisation</li>
+                    <li>Web Analytics Setup</li>
+                    <li>Sales Funnel Iterations/Testing</li>
+             			
+             		</ul>
+             	</div>
+             </div>
+
+
+             </div>
+
+		  	</div>
+		  </div>
+
+
+		</section>
+
+		<section id="contact">
+			<div class="container">
+		  	<div class="content">
+			<h1>Get in touch</h1>
+
+			<button id="front-page-btn" class="btn btn-primary">Get in Touch</button>
+			
+
+
+		</div>
+	</div>
+		</section>
+		<footer>
+			<div class="container">
+			<ul>
+				<li><a href="">Back to Top</a></li>
+				<li><a href="">Contact</a></li>
+				<li><a href="">Contact</a></li>
+			</ul>
+			</div>
+		</footer>
 
 
 	</body>
